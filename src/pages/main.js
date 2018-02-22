889bcda7d9554f1444d1e6eef52a83b23be556c6
志成 <xuzhicheng@raycloud.com>
2018-02-22 11:12:30 +0800

    icons =》 ok

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index'
+import router from './routers'
 import comps from './plaugin'
 
 Vue.use(comps);
--- src/pages/routers.js
+++ src/pages/routers.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import index from '../index/index.vue'
+import index from './index/index.vue'
+import icon from './icon/index.vue'
 
 Vue.use(Router);
 
@@ -11,6 +12,10 @@ export default new Router({
           path:'/',
           name:'index',
           component:index
+      },{
+          path:'/icon',
+          name:'icon',
+          component:icon
       }
   ]
 })
