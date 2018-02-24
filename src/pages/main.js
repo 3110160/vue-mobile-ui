9341d22333b4ba2848f2c5ef8e05dc51b2025749
志成 <xuzhicheng@raycloud.com>
2018-02-24 11:27:06 +0800

    css reset => ok

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,6 +5,10 @@ import comps from './plaugin'
 
 Vue.use(comps);
 
+window.document.addEventListener('touchstart', function (event) {
+    /* 解决 active兼容处理 即 伪类 :active 失效  */
+}, false);
+
 Vue.config.productionTip = false; //关闭pro 环境提示
 new Vue({
   el: '#app',
