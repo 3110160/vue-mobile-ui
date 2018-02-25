64fcd1b4460a8a3f9e2e781785f63476ea0867a0
志成 <xuzhicheng@raycloud.com>
2018-02-25 16:37:31 +0800

    优化button => ok

--- build/utils.js
+++ build/utils.js
@@ -16,7 +16,7 @@ exports.cssLoaders = function (options) {
   options = options || {}
 
   const cssLoader = {
-    loader: 'css-loader',
+    loader: 'css-loader',//?modules&localIdentName=[name]__[local]--[hash:base64:5]'
     options: {
       sourceMap: options.sourceMap
     }
